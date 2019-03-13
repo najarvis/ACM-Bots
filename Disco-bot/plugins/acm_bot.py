74a562151218082d9061f0320fa2a2f383bb887b
Nick Jarvis <najarvis2016@gmail.com>
2019-03-13 17:21:44 -0400

    Adds more functionality to disco bot

--- Disco-bot/plugins/acm_bot.py
+++ Disco-bot/plugins/acm_bot.py
@@ -9,6 +9,20 @@ class ACMPlugin(Plugin):
     def command_ping(self, event):
         event.msg.reply('Pong!')
 
-    @Plugin.command('award', '<name:str> <num:int>', grouop='points')
+    @Plugin.command('award', '<name:str> <num:int>', group='points')
     def on_award_command(self, event, name, num):
-        db.insert()
+        event.msg.reply("Awarding {} points to {}".format(name, num))
+        points = db.table('points')
+        User = Query()
+        document = points.get(User.name == name)
+        if document is not None:
+            points.update({'points': document.points + num})
+        else:
+            points.insert({'name': name, 'points': num})
+
+    @Plugin.command('list', group='points')
+    def on_list_points_command(self, event):
+        points = db.table('points')
+        event.msg.reply("Point Listing:")
+        for document in points.all():
+            event.msg.reply("\t{}: {}".format(document['name'], document['points']))
